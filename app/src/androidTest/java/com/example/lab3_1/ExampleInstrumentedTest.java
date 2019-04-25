381bb808d9becf86f6c21893a9af07a28af97b2e
Николаев Никита <43088847+WAndromeda@users.noreply.github.com>
2019-04-25 23:11:49 +0300

    Lab3_1

--- app/src/androidTest/java/com/example/lab3_1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lab3_1/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.lab_one;
+package com.example.lab3_1;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.lab_one", appContext.getPackageName());
+        assertEquals("com.example.lab3_1", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/lab3_1/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab3_1/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.lab_one;
+package com.example.lab3_1;
 
 import org.junit.Test;
 
